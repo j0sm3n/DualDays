04a8523137ea6e936bee10ec1da4e23a0a144597
Jose Antonio Mendoza <66326890+j0sm3n@users.noreply.github.com>
2025-03-14 18:30:25 +0100

    Initial Commit

---
+++ DualDays/DualDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  DualDaysApp.swift
+//  DualDays
+//
+//  Created by Jose Antonio Mendoza on 14/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DualDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
